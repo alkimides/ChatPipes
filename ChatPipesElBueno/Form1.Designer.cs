55752ea09aa90e121f1641f7cc436809e64a5dbd
alkimides <inaki.seim@gmail.com>
2019-11-20 13:02:37 +0100

    chat pipes 1

---
+++ ChatPipesElBueno/Form1.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace ChatPipesElBueno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMsg = new System.Windows.Forms.TextBox();
+            this.txtChat = new System.Windows.Forms.TextBox();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMsg
+            // 
+            this.txtMsg.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMsg.Location = new System.Drawing.Point(55, 326);
+            this.txtMsg.Multiline = true;
+            this.txtMsg.Name = "txtMsg";
+            this.txtMsg.Size = new System.Drawing.Size(414, 42);
+            this.txtMsg.TabIndex = 0;
+            // 
+            // txtChat
+            // 
+            this.txtChat.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtChat.Location = new System.Drawing.Point(55, 58);
+            this.txtChat.Multiline = true;
+            this.txtChat.Name = "txtChat";
+            this.txtChat.Size = new System.Drawing.Size(414, 236);
+            this.txtChat.TabIndex = 1;
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Location = new System.Drawing.Point(510, 326);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(80, 44);
+            this.btnEnviar.TabIndex = 2;
+            this.btnEnviar.Text = "Enviar";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 420);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.txtChat);
+            this.Controls.Add(this.txtMsg);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMsg;
+        private System.Windows.Forms.TextBox txtChat;
+        private System.Windows.Forms.Button btnEnviar;
+    }
+}
+
---
+++ Microfono/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.IO.Pipes;
+using System.Diagnostics;
+using System.Configuration;
+
+namespace Microfono
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NamedPipeClientStream npcs = new NamedPipeClientStream(".", "microfono", PipeDirection.In);
+            NamedPipeServerStream npss = new NamedPipeServerStream("mic-aur", PipeDirection.Out);
+
+            //conectamos el cliente
+            npcs.Connect();
+            //servidor esperando conexion
+            npss.WaitForConnection();
+
+            //creamos un reader para el cliente y un writer para el servidor
+            StreamReader sr = new StreamReader(npcs);
+            StreamWriter sw = new StreamWriter(npss);
+
+            //creamos el mensaje
+            string mensaje = sr.ReadLine();
+            sw.WriteLine(mensaje);
+        }
+    }
+}
